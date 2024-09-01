16c83ee294b5f24d1bcf64eff3951df7e5af6bda
seanoh1014 <103906443+seanoh1014@users.noreply.github.com>
2024-09-01 18:53:42 -0500

    Create Runner.java

---
+++ Runner.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        // Write code to test your MyArrayList methods
+        MyArrayList list = new MyArrayList();
+
+        list.add("hello");
+        list.add("Java");
+        list.add("test");
+        list.add("world");
+        list.add("Java");
+        list.add("test");
+        System.out.println(list);
+        System.out.println(list.remove("test"));
+        System.out.println(list);
+      
+
+
+
+      
+  }
+}
